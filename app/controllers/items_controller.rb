e977d74be657998f2c81dc0fe70bd0d84958a947
matsumotoyousuke <k_d_k_knizg@yahoo.co.jp>
2023-10-07 15:04:36 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if user_signed_in?
+      if current_user.id == @item.user.id
+        @item = Item.find(params[:id])
+      else
+        redirect_to root_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_paramas)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_paramas
