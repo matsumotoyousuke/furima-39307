03cd58f0b8142268af8374701abd2d1e4d938c8c
matsumotoyousuke <k_d_k_knizg@yahoo.co.jp>
2023-10-12 10:45:53 +0900

    挙動確認後の修正(1)

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -3,13 +3,12 @@ window.addEventListener('load', () => {
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
-      const addTaxDom = document.getElementById("add-tax-price");
       const Tax = 0.1
-      const priceAfterTax = inputValue * Tax
-      addTaxDom.innerHTML = Math.floor(priceAfterTax)
-
+      const priceAfterTax = Math.floor(inputValue * Tax)
+      const addTaxDom = document.getElementById("add-tax-price");
       const profitPrice = document.getElementById("profit");
-      profitPrice.innerHTML = Math.floor(inputValue - priceAfterTax)
+      addTaxDom.innerHTML = priceAfterTax
+      profitPrice.innerHTML = inputValue - priceAfterTax
   })
 
   
